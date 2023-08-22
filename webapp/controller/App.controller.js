faa799b62957d90a656935b48ab0995716299bf4
dattavijit <37737599+dattavijit@users.noreply.github.com>
2023-08-22 14:10:28 +0930

    8. Translatable Texts

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast", 
-	"sap/ui/model/json/JSONModel"
-], function( Controller, MessageToast, JSONModel) {
+	"sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+], function( Controller, MessageToast, JSONModel, ResourceModel) {
 	"use strict";
 
 	return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
@@ -14,10 +15,21 @@ sap.ui.define([
             };
             var oModel = new JSONModel(oData);
             this.getView().setModel(oModel);
+
+            var i18nModel = new ResourceModel({
+                bundleName: "sap.ui.demo.walkthrough.i18n.i18n", 
+                supportedLocales: [""],
+                fallbackLocale: ""
+            });
+            this.getView().setModel(i18nModel, "i18n");
         },
 
         onSayHiButtonPress: function(){
-            new MessageToast.show("Hi");
+            //read message from i18n model
+            var oBundle = this.getView().getModel("i18n").getResourceBundle();
+            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+            new MessageToast.show("Hello " + sRecipient + "! ");
         }
 
 	});
