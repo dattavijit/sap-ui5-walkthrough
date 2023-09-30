decd03f27fab4789c282eb98e3d0bcaf6dfa48a4
dattavijit <37737599+dattavijit@users.noreply.github.com>
2023-09-30 14:57:19 +0930

    Step 23: Filtering

--- webapp/controller/Invoice.controller.js
+++ webapp/controller/Invoice.controller.js
@@ -1,11 +1,15 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    "../model/formatter"
+    "../model/formatter", 
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
 ], function (
     Controller,
     JSONModel,
-    formatter
+    formatter, 
+    Filter,
+    FilterOperator
 ) {
     "use strict";
 
@@ -20,6 +24,19 @@ sap.ui.define([
             });
             this.getView().setModel(oViewModel, "view");
 
+        }, 
+        onFilterInvoices: function(oEvent){
+            //Builf filter array
+            var aFilter = [];
+            var sQuery = oEvent.getParameter("query");
+            if (sQuery){
+                aFilter.push(new Filter("ProductName", FilterOperator.Contains , sQuery));
+            }
+
+            //Filter binding
+            var oList = this.byId("invoiceList");
+            var oBinding = oList.getBinding("items");
+            oBinding.filter(aFilter);
         }
     });
 
